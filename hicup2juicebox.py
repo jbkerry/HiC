3984f09f6018fcb30dbc86ebd720c84c2a9faa29
Jon Kerry <jon.kerry@imm.ox.ac.uk>
2017-01-24 17:30:31 +0000

    end of day 24-01-17

--- hicup2juicebox.py
+++ hicup2juicebox.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
-import subprocess
+#import subprocess
 
-#import pysam
-#samfile = pysam.AlignmentFile("/t1-data1/WTSA_Dev/jkerry/HiC/HiCUP/hicup_v0.5.9/Mifsud2015/CD34_HiC_1_2.hicup.sorted.bam", "rb")
+import pysam
+samfile = pysam.AlignmentFile("/t1-data1/WTSA_Dev/jkerry/HiC/HiCUP/hicup_v0.5.9/Mifsud2015/CD34_HiC_1_2.hicup.bam", "rb")
 #
 #variable = samfile.fetch("chr1", 10000, 200000)
 #for x in variable:
 #    print (str(x))
     
-samfile = "/t1-data1/WTSA_Dev/jkerry/HiC/HiCUP/hicup_v0.5.9/Mifsud2015/CD34_HiC_1_2.hicup.bam"
-
-ViewFile = subprocess.Popen("samtools view "+samfile, shell=True, stdout=subprocess.PIPE)
-#FileToVar = ViewFile.stdout.read().rstrip('\n')
-FileToVar = ViewFile.stdout.read()
+#samfile = "/t1-data1/WTSA_Dev/jkerry/HiC/HiCUP/hicup_v0.5.9/Mifsud2015/CD34_HiC_1_2.hicup.bam"
+#
+#ViewFile = subprocess.Popen("samtools view "+samfile, shell=True, stdout=subprocess.PIPE)
+##FileToVar = ViewFile.stdout.read().rstrip('\n')
+##FileToVar = ViewFile.stdout.read()
+#
+#samlines = [samlineread.rstrip('\n') for samlineread in ViewFile.stdout.read()]
+#for ThisSAMline in samlines:
+#    parts = ThisSAMline.split("\t")
+    
+for read in samfile.fetch(until_eof=True):
+    print(read)
