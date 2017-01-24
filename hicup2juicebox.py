46cf2e32e9c31c941213fa692ae83c46f31d3a7d
Jon Kerry <jon.kerry@imm.ox.ac.uk>
2017-01-24 17:01:07 +0000

    created file hicup2juicebox.py

--- hicup2juicebox.py
+++ hicup2juicebox.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-import pysam
-samfile = pysam.AlignmentFile("/t1-data1/WTSA_Dev/jkerry/HiC/HiCUP/hicup_v0.5.9/Mifsud2015/CD34_HiC_1_2.hicup.bam", "rb")
+import subprocess
 
-variable = samfile.fetch("chr1", 10, 20)
-for x in variable:
-    print (str(x))
+#import pysam
+#samfile = pysam.AlignmentFile("/t1-data1/WTSA_Dev/jkerry/HiC/HiCUP/hicup_v0.5.9/Mifsud2015/CD34_HiC_1_2.hicup.sorted.bam", "rb")
+#
+#variable = samfile.fetch("chr1", 10000, 200000)
+#for x in variable:
+#    print (str(x))
+    
+samfile = "/t1-data1/WTSA_Dev/jkerry/HiC/HiCUP/hicup_v0.5.9/Mifsud2015/CD34_HiC_1_2.hicup.bam"
+
+ViewFile = subprocess.Popen("samtools view "+samfile, shell=True, stdout=subprocess.PIPE)
+#FileToVar = ViewFile.stdout.read().rstrip('\n')
+FileToVar = ViewFile.stdout.read()
